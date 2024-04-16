3fc21b7dc8d46da2ebce2a1c6377395c18b36879
Billy Dunn <bdunn27@gmail.com>
2024-04-16 12:26:11 -0400

    missing syntax

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return;
-  <nav>
-    <NavLink to="/" className="nav-link">
-      Home;
-    </NavLink>
-    <NavLink to="/BreakfastPage" className="nav-link">
-      Breakfast
-    </NavLink>
-    <NavLink to="/Brunch" className="nav-link">
-      Brunch
-    </NavLink>
-    <NavLink to="/Lunch" className="nav-link">
-      Lunch
-    </NavLink>
-    <NavLink to="/ReviewPage" className="nav-link">
-      Reviews
-    </NavLink>
-  </nav>;
+  return (
+    <nav>
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/BreakfastPage" className="nav-link">
+        Breakfast
+      </NavLink>
+      <NavLink to="/Brunch" className="nav-link">
+        Brunch
+      </NavLink>
+      <NavLink to="/Lunch" className="nav-link">
+        Lunch
+      </NavLink>
+      <NavLink to="/ReviewPage" className="nav-link">
+        Reviews
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;
