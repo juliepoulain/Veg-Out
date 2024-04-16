42105e43fe5c84485ffddaaa28bac71794a17c02
Billy Dunn <bdunn27@gmail.com>
2024-04-16 11:07:21 -0400

    route and nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <nav className="navbar">navbar</nav>;
+  return;
+  <nav>
+    <NavLink to="/" className="nav-link">
+      Home;
+    </NavLink>
+    <NavLink to="/BreakfastPage" className="nav-link">
+      Breakfast
+    </NavLink>
+    <NavLink to="/Brunch" className="nav-link">
+      Brunch
+    </NavLink>
+    <NavLink to="/Lunch" className="nav-link">
+      Lunch
+    </NavLink>
+    <NavLink to="/ReviewPage" className="nav-link">
+      Reviews
+    </NavLink>
+  </nav>;
 }
 
 export default NavBar;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 import ReviewsPage from "./components/ReviewsPage";
-import Home from "./components/Home"
+import Home from "./components/Home";
 import ErrorPage from "./components/ErrorPage";
+import NavBar from "./components/NavBar";
+import BreakfastPage from "./components/BreakfastPage";
+import Brunch from "./components/Brunch";
+import ReviewForm from "./components/ReviewForm";
+import Lunch from "./components/Lunch";
 
 const routes = [
   {
@@ -10,7 +15,28 @@ const routes = [
   {
     path: "/reviews/:id",
     element: <ReviewsPage />,
-  }
+  },
+  {
+    path: "/reviewForm:/id",
+    element: <ReviewForm />,
+  },
+  {
+    path: "/BreakfastPage",
+    element: <BreakfastPage />,
+  },
+  {
+    path: "/Brunch",
+    element: <Brunch />,
+  },
+  {
+    path: "/Lunch",
+    element: <Lunch />,
+  },
+
+  {
+    path: "/NavBar",
+    element: <NavBar />,
+  },
 ];
 
 export default routes;
