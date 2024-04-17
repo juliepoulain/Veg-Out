9f98533f873a8ef76e7fb0348e6e1c9b8d92b060
Julie Poulain <juliempoulain@gmail.com>
2024-04-17 14:00:53 -0700

    syntax

--- src/components/BreakfastPage.js
+++ src/components/BreakfastPage.js
@@ -1,18 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import BreakfastList from "./BreakfastList";
 import NavBar from "./NavBar";
-import Search from "./Search"
+import Search from "./Search";
 
 function BreakfastPage() {
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
   const [menuItems, setMenuItems] = useState([]);
 
   return (
     <div className="menu">
       <NavBar />
       <h2>Breakfast Menu</h2>
-      <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
-      <BreakfastList 
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      <BreakfastList
         menuItems={menuItems}
         setMenuItems={setMenuItems}
         searchValue={searchValue}
