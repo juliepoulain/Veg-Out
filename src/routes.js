e59dca2888f57d14ec8de56b388ef24ee8a1280b
Julie Poulain <juliempoulain@gmail.com>
2024-04-17 11:14:28 -0700

    updatedb

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import ReviewsPage from "./components/ReviewsPage";
 import Home from "./components/Home";
 import ErrorPage from "./components/ErrorPage";
-import NavBar from "./components/NavBar";
 import BreakfastPage from "./components/BreakfastPage";
 import BrunchPage from "./components/BrunchPage";
 import ReviewForm from "./components/ReviewForm";
@@ -11,31 +10,32 @@ const routes = [
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/reviews/:id",
     element: <ReviewsPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/reviewForm/:id",
     element: <ReviewForm />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/BreakfastMenu",
     element: <BreakfastPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/BrunchMenu",
     element: <BrunchPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/LunchMenu",
     element: <LunchPage />,
-  },
-
-  {
-    path: "/NavBar",
-    element: <NavBar />,
+    errorElement: <ErrorPage />
   },
 ];
 
